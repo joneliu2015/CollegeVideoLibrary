edf30d125cc134fa0e469f4111925b91ec67bc1c
liujun <1028301097@qq.com>
2017-12-08 19:07:40 +0800

    12.8再次提交

--- js/index.js
+++ js/index.js
@@ -1,35 +1,24 @@
-(function(){
-
-// $("li").on("cilck","a",function(){
-//     $(this).addClass('active').siblings().removeClass('active');
-// });
-$(".country>li").on("click",function(){
-    $(this).addClass('active').siblings().removeClass('active');
-    // alert("被电击了")
-});
-   //获取当前tab中鼠标停在的table栏的索引  
-   var index = $(this).index();  
-   //  console.log(index);  
-   //根据这个索引切换，下面的展示  
-   $(".main").eq(index).addClass("selected").siblings().removeClass("selected");  
+//(function(){
+//
+//
+//$(".country>li").on("click",function(){
+//  $(this).addClass('active').siblings().removeClass('active');
+//  
+//  
+//  
+//  // alert("被电击了")
+//  
+//  
+//  //获取当前tab中鼠标停在的table栏的索引  
+// var index = $(this).index();  
+//     console.log(index);  
+// //根据这个索引切换，下面的展示  
+// $("li").eq(index).addClass("selected").siblings().removeClass("selected");  
+// 
+//});
    
    
    
-   
-   
-   
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-})()
+//})()
