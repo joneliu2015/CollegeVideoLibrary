83f9cade1ea52b86aec8fc4ad5d1aa5fd18ef745
liujun <1028301097@qq.com>
2017-12-21 18:14:14 +0800

    整理代码格式

--- js/index.js
+++ js/index.js
@@ -8,61 +8,39 @@ window.onload = function() {
 		success: function(data, key) {
 			//转化为js对象
 			var jsArr = data;
-
-			//包装为js对象
-
-
 			//遍历JSON对象，将JSON对象转化为数组
-			for(var key in jsArr) {
-			
-			};
-
+			for(var key in jsArr) {};
 			//点击国家按钮显示指定内容
 			$("body").on("click", ".country>li", function(e) {
-
 				$(this).addClass('active').siblings().removeClass('active');
-
 				// alert("被电击了")
-
 				//获取当前tab中鼠标停在的table栏的索引  
 				var index = $(this).index();
-		
 				//根据这个索引切换，下面的展示  
 				$("li").eq(index).addClass("selected").siblings().removeClass("selected");
-
 				//获取当前点击项目的ID
 				var indexId = $(this).attr('id');
-			
-
 				//新建数组，将当前点击项的id赋值给当前要显示的数组作为KEY值，就获取的当前显示项的数据
-				
-
 				var jsArri = jsArr[this.id];
-
 				if(jsArri === undefined || jsArri.length == 0) {
 					$('.medialist').html('<img class="no-content" style="" src="img/no_content.png" alt="" />');
 					$('#page').hide();
 				} else {
 					$('#page').show();
 				}
-
 				// 分页
 				$('#page').html("");
 				var pageData = jsArri.length;
-			
 				$(function() {
 					var count = pageData; //设置总条数
-				
 					var pageSize = 5; //每页显示的条数
 					var pageCount = Math.ceil(count / pageSize); //能显示几页
-				
 					var currentPage = 1; //默认显示第一页
 					//做个分页按钮
 					for(var i = 1; i <= pageCount; i++) {
 						var pageN = '<span class="pageName"  selectPage="' + i + '">' + i + '</span>'
 						$('#page').append(pageN);
 					}
-
 					//默认显示第一页
 					$('.medialist').html('');
 					for(i = (currentPage - 1) * pageSize; i < pageSize * currentPage; i++) {
@@ -73,7 +51,6 @@ window.onload = function() {
 						$('.medialist').append(result);
 						$('#page span:first-child').addClass('choosed')
 					};
-
 					//显示选择页的内容  
 					$('.pageName').click(function() {
 						var selectPage = $(this).attr('selectPage');
@@ -135,7 +112,7 @@ window.onload = function() {
 							time: 500
 						})
 						//Layer弹窗结束
-					}									
+					}
 				});
 			});
 			//加载第一项美国
